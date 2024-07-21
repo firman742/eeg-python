01eb68c6dbb691d925740ae63f2937092db1bb47
firman742 <hafidzfirman742@gmail.com>
2024-07-21 21:53:02 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st 
+
+# ---- Page Setup -----
+
+dashboard_page = st.Page(
+        page="views/dashboard.py",
+        title="Dashboard",
+        icon=":material/bar_chart:",
+        default=True
+)
+
+analysis_page = st.Page(
+        page="views/analysis.py",
+        title="Analysis",
+        icon=":material/account_circle:",
+)
+
+about_page = st.Page(
+        page="views/about.py",
+        title="About",
+        icon=":material/account_circle:",
+)
+#  --- Navigation Setup ---
+pg = st.navigation(pages=[dashboard_page, analysis_page, about_page])
+
+
+# Shared On All pages
+st.sidebar.text("Copyright @2024 ...")
+pg.run()
